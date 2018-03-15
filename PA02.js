bf3354b69228abf94acd897232202997cb8cfda6
jenn14108 <jelee14108@github.com>
2018-03-15 17:34:29 -0400

    kkkk

--- PA02.js
+++ PA02.js
@@ -193,12 +193,9 @@
 		// gudetama.position.x = 15;
 		// gudetama.position.y = 2;
 		// gudetama.position.z = 10;
-<<<<<<< HEAD
 		gudetama.position.set(randN(20)+15,30,randN(20)+15);
 
-=======
 		gudetama.position.set(-40,40,-40);
->>>>>>> origin/master
 		scene.add(gudetama);
 
 		gudetama.addEventListener('collision',
